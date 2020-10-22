00c31bc688b82562c1953def9ea94c04151b6716
zdanielz <43722126+zdanielz@users.noreply.github.com>
2020-10-22 23:57:02 +0300

    ОБНОВА!!! (ПРЕД ФАЙНАЛ ВЕРСИОН)
    добавлены счетчик, обработчик столкновений, убраны и заменены участки кода приводящие к  критическим повреждениям памяти

--- код/Source.cpp
+++ код/Source.cpp
@@ -1,10 +1,12 @@
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:WinMainCRTStartup")
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
 #include <iostream>
 #include <random>
 #include <thread>
 #include <time.h>
+#include <vector>
 #include "Snake.h"
 using namespace std;
 
@@ -13,12 +15,15 @@ const int padding = 1;     //отступ между частями поля
 
 Snake snake = Snake(GetSystemMetrics(SM_CXSCREEN) / (SizeOfPart + padding), GetSystemMetrics(SM_CYSCREEN) / (SizeOfPart + padding));
 coord SnakeDirection = { -1, 0 }; //направление змейки
+HWND hMainWnd;
 
 int GameProc(int x_, int y_, int width, int height, int padding, int SizeOfPart, COLORREF mainColor, COLORREF AppleCollor, COLORREF snakeColor, HDC dc, HDC dcCompatible, HBITMAP hbm) {
 	HBRUSH hBrush;
 	bool game = true;
 	bool appleExist = false;
 	Apple apple;
+	int score = 0;
+	char buf1[32];
 
 	while (game) {
 		hBrush = CreateSolidBrush(mainColor);
@@ -30,7 +35,7 @@ int GameProc(int x_, int y_, int width, int height, int padding, int SizeOfPart,
 		} // создание поля квадратов
 		DeleteObject(hBrush);
 		if (!appleExist) {
-			apple = Apple(rand() % x_, rand() % y_);
+			apple = Apple(1 + rand() % (x_ - 1), 1 + rand() % (y_ - 1));
 			hBrush = CreateSolidBrush(AppleCollor);
 			SelectObject(dcCompatible, hBrush);
 			CreatRect(dcCompatible, apple.coordAple, SizeOfPart, x_, y_, padding, hbm); //отрисовка яблока
@@ -49,20 +54,46 @@ int GameProc(int x_, int y_, int width, int height, int padding, int SizeOfPart,
 		if (snake.snake_mas[0] == apple.coordAple) {
 			apple.~Apple();
 			snake.SneakEatApple();
+			score++;
 			appleExist = false;
 		}
 		for (int i = 0; i < snake.SizeOfSnake(); i++) {
 			CreatRect(dcCompatible, snake.snake_mas[i], SizeOfPart, x_, y_, padding, hbm);
 		}  // отрисовка змейки
 		DeleteObject(hBrush);
+
+		if (snake.snake_mas[0].x == -1 |
+			snake.snake_mas[0].x == x_ - (x_ / 11) |
+			snake.snake_mas[0].y == -1 |
+			snake.snake_mas[0].y == y_ - (y_ / 10)) { game = false; } //game over если зейка ударилась об край
+
+		for (int i = 1; i < snake.SizeOfSnake(); i++) {
+			if (snake.snake_mas[0] == snake.snake_mas[i]) { game = false; }
+		} //game over если зейка ударилась об себя
+
+		sprintf(buf1, "SCORE : %i\x00", score);
+		DrawText(dcCompatible, buf1, -1, &tagRECT({ width - 100, 0, width, height }), DT_CENTER);
 		BitBlt(dc, 0, 0, width, height, dcCompatible, 0, 0, SRCCOPY);
 		Sleep(50);
 	}
-	snake.~Snake();
+	DrawText(dcCompatible, "GAME OVER", -1, &tagRECT({ 0 - (width / 40), height / 2 - 50, width, height }), DT_CENTER);
+	
+	for (int i = 0; i < 10; i++) {
+		BitBlt(dc, 0, 0, width, height, dcCompatible, 0, 0, SRCCOPY);
+		Sleep(100);
+	}
+
+	for (int i = 1; i < 3000; i++) {
+		DrawText(dcCompatible, "GAME OVER", -1, &tagRECT({ rand() % (width * 2) - width, rand() % height, width, height }), DT_CENTER);
+		BitBlt(dc, 0, 0, width, height, dcCompatible, 0, 0, SRCCOPY);
+		Sleep(3000 / (i * 3));
+	}
+	MessageBox(NULL, "!GAME OVER!", NULL, MB_OK);
+	PostMessage(hMainWnd, WM_QUIT, NULL, NULL);
 	return 0;
 }
 
-bool FullScreen = false;
+bool FullScreen = true;
 
 LRESULT CALLBACK WindowProc(
 	HWND hwnd,
@@ -90,13 +121,12 @@ int WINAPI WinMain(
 	height = GetSystemMetrics(SM_CYSCREEN);  //высота экрана
 
 	TCHAR szClassName[] = "Мой класс"; // строка с именем класса
-	HWND hMainWnd; // создаём дескриптор будущего окошка
 	MSG msg; // создём экземпляр структуры MSG для обработки сообщений
 	WNDCLASSEX wc; // создаём экземпляр, для обращения к членам класса WNDCLASSEX
 	wc.cbSize = sizeof(wc); // размер структуры (в байтах)
 	wc.style = CS_HREDRAW | CS_VREDRAW; // стиль класса окошка
 	wc.lpfnWndProc = WndProc; // указатель на пользовательскую функцию
-	wc.lpszMenuName = NULL; // указатель на имя меню (у нас его нет)
+	wc.lpszMenuName = NULL; // указатель на имя меню
 	wc.lpszClassName = szClassName; // указатель на имя класса
 	wc.cbWndExtra = NULL; // число освобождаемых байтов в конце структуры
 	wc.cbClsExtra = NULL; // число освобождаемых байтов при создании экземпляра приложения
@@ -113,8 +143,8 @@ int WINAPI WinMain(
 	// Функция, создающая окошко:
 	hMainWnd = CreateWindow(
 		szClassName, // имя класса
-		"Змейка", // имя окошка (то что сверху)
-		WS_OVERLAPPEDWINDOW | WS_POPUP, // режимы отображения окошка
+		"Змейка", // имя окошка
+		WS_POPUP, // режимы отображения окошка
 		CW_USEDEFAULT, // позиция окошка по оси х
 		NULL, // позиция окошка по оси у (раз дефолт в х, то писать не нужно)
 		CW_USEDEFAULT, // ширина окошка
@@ -129,7 +159,7 @@ int WINAPI WinMain(
 		MessageBox(NULL, "Не получилось создать окно!", "Ошибка", MB_OK);
 		return NULL;
 	}
-	ShowWindow(hMainWnd, SW_MAXIMIZE); // отображаем окошко
+	ShowWindow(hMainWnd, SW_SHOWMAXIMIZED); // отображаем окошко
 	UpdateWindow(hMainWnd); // обновляем окошко
 
 	dc = GetDC(hMainWnd); //получение контекста устройства монитора
@@ -144,16 +174,18 @@ int WINAPI WinMain(
 	y_ = height / (SizeOfPart + padding); //размер поля в... каких то еденицах
 
 	thread GameThread(GameProc, x_, y_, width, height, padding, SizeOfPart, mainColor, appleCollor, snakeColor, dc, dcCompatible, hbm);
-	GameThread.detach();
-
+	
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg); //Преобразуем сообщения
 		DispatchMessage(&msg); //Передаём сообщение соответствующей функции окна
 	}
 
+	GameThread.detach();
+	snake.~Snake();
+	DeleteObject(dc);
 	DeleteObject(dcCompatible);               //  УБОРКА МУСОРА  //
 
-	return 0; // возвращаем код выхода из приложения
+	return 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
